af772a04780301ef0a5192af6823605fd0b1a886
williamristorp <william@heidelberg.nu>
2026-02-19 12:24:41 +0100

    Remove LibStub dependency

--- LibCLI.lua
+++ LibCLI.lua
@@ -3,17 +3,3 @@ addon = addon or {}
 
 local libcli = addon.libcli or {}
 addon.libcli = libcli
-
-if LibStub then
-    local LIBSTUB_MAJOR, LIBSTUB_MINOR = "LibCLI-1.0", 0
-    local lib = LibStub:NewLibrary(LIBSTUB_MAJOR, LIBSTUB_MINOR)
-    if not lib then
-        return
-    end
-
-    for _, key in ipairs(libcli) do
-        if key ~= "internal" then
-            lib[key] = libcli[key]
-        end
-    end
-end
